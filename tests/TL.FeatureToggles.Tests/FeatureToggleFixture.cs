85c364a2c66706cef7d723987436ae03b8eb1bbd
Thiago Lunardi <thiago.lunardi@complevo.de>
2018-11-30 12:44:46 +0100

    Adding missing tests

---
+++ tests/TL.FeatureToggles.Tests/FeatureToggleFixture.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xunit;
+
+namespace TL.FeatureToggles.Tests
+{
+    [CollectionDefinition(nameof(FeatureToggleCollection))]
+    public class FeatureToggleCollection : ICollectionFixture<FeatureToggleFixture>
+    {
+    }
+
+    public class FeatureToggleFixture : IDisposable
+    {
+        public IToggleRouter ToggleRouter { get; }        
+
+        public FeatureToggleFixture()
+        {
+            ToggleRouter = new ToggleRouter();
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
